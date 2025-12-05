4a9dd14c9f0b2fb67b60bf06a95e15348cefaa9c
Rafael Valverde Fonseca <rafaelvalverde.dev@gmail.com>
2025-12-05 12:02:49 -0300

    Atualização 05/12/2025

--- ecc-backend/src/routes/inscricoes.routes.js
+++ ecc-backend/src/routes/inscricoes.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import {
   criarInscricao,
   atualizarInscricao,
+  listarInscricoes
 } from "../controllers/inscricoes.controller.js";
 
 const router = Router();
 
 router.post("/", criarInscricao);
 router.put("/:id", atualizarInscricao);
-
+router.get("/", listarInscricoes);
 export default router;
