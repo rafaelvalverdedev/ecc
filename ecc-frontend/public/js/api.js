6a205b4da4943a23d67ee64c4b221cbeb6dcf31c
Rafael Valverde Fonseca <rafaelvalverde.dev@gmail.com>
2025-12-26 18:29:08 -0300

    ....

--- ecc-frontend/public/js/api.js
+++ ecc-frontend/public/js/api.js
@@ -1,7 +1,9 @@
 function api(url, options = {}) {
   const token = localStorage.getItem("token");
 
-  return fetch("http://localhost:3001" + url, {
+//    return fetch("http://127.0.0.1/3001" + url, {
+
+  return fetch("https://ecc-backend-8i9l.onrender.com" + url, {
     ...options,
     headers: {
       "Content-Type": "application/json",
