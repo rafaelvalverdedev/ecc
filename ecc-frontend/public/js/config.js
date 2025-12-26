ea35e98528e02594bfa29bb43b0417db8f8cedbd
Rafael Valverde Fonseca <rafaelvalverde.dev@gmail.com>
2025-12-26 18:49:42 -0300

    ....

--- ecc-frontend/public/js/config.js
+++ ecc-frontend/public/js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  API_BASE_URL: "http://localhost:3001" // testes local
-  // API_BASE_URL: "https://ecc-backend-8i9l.onrender.com" // producao 
+  // API_BASE_URL: "http://localhost:3001" // testes local
+  API_BASE_URL: "https://ecc-backend-8i9l.onrender.com" // producao 
 };
 
 // ====================================
