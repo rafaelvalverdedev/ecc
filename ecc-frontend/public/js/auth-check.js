a3197b64f7dc0878b10b9b3069df5c89a801e932
Rafael Valverde Fonseca <rafaelvalverde.dev@gmail.com>
2025-12-29 16:34:09 -0300

    .....

--- ecc-frontend/public/js/auth-check.js
+++ ecc-frontend/public/js/auth-check.js
@@ -1,12 +1,21 @@
 // ======================================
 // 🔐 Autenticação
 // ======================================
-const token = localStorage.getItem("token");
-const user = JSON.parse(localStorage.getItem("user"));
 
 function requireAuth() {
   const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user"));
   if (!token) {
-    window.location.href = "../auth";
+   alert(window.location.href = `${window.location.origin}/auth`);
   }
 }
+
+// ====================================
+// 🔓 Logout
+// ====================================
+function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+    window.location.href = `${window.location.origin}/auth`;
+}
+
