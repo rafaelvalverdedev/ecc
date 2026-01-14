a0f5795b41ce79603c3d70e0acada3058adff6e4
Rafael Valverde Fonseca <rafaelvalverde.dev@gmail.com>
2026-01-14 16:41:48 -0300

    Navbar concluida

--- ecc-frontend/public/components/navbar.js
+++ ecc-frontend/public/components/navbar.js
@@ -9,62 +9,61 @@ export function renderNavbar({ active }) {
     { page: "equipes", label: "Equipes" },
     { page: "cadastro", label: "Cadastro" }
   ];
-  
+
   container.innerHTML = `
-  <nav>
-    <ul>
-        // <li class="logotipo">
-        //     <img src="../assets/Logo-v2-2025-logo.png" with="120" alt="Logotipo ECC">
-        // </li>
+    <nav>
+      <ul>
+        <li class="logotipo">
+             <img src="../assets/Logo-v2-2025-logo.png" width="120" alt="Logotipo ECC">
+        </li>
 
         <li class="hideMobile">
           ${links
+            .map(
+              ({ page, label }) => `
+                <button
+                  class="nav-btn ${page === active ? "active" : ""}"
+                  data-page="${page}">
+                  ${label}
+                </button>
+              `
+            )
+            .join("")}
+          <button class="btn-logout">Sair</button>
+        </li>
+
+        <li class="hideDesktop" onclick="showSidebar()">
+          <a href="#">
+            <img src="../assets/menu-com.svg" width="20" alt="Abrir Menu" style="margin-right: 20px;">
+          </a>
+        </li>
+      </ul>
+
+      <ul class="sidebar">
+        <li class="fechar" onclick="hideSidebar()">
+          <a href="#">
+            <img src="../assets/close.svg" width="30" alt="Fechar Menu" style="margin-right: 20px;">
+          </a>
+        </li>
+
+        ${links
           .map(
             ({ page, label }) => `
-                          <button
-                            class="nav-btn ${page === active ? "active" : ""}"
-                            data-page="${page}">
-                            ${label}
-                          </button>
-                          `
+              <li>
+                <button
+                  class="nav-btn ${page === active ? "active" : ""}"
+                  data-page="${page}">
+                  ${label}
+                </button>
+              </li>
+            `
           )
-          
           .join("")}
-          <button class="logout-btn" id="btn-logout">Sair</button>
-      </li>
 
-      <li class="hideDesktop" onclick="showSidebar()">
-        <a href="#">
-        <img src="../assets/menu-com.svg" width="30" alt="Fechar Menu" style="margin-right: 20px;">
-        </a>
-        </li>       
-        </ul>
-        
-        <ul class="sidebar">
-        <li class="fechar" onclick=hideSidebar()>
-        <a href="#">
-            <img src="../assets/close.svg" width="30" alt="Fechar Menu" style="margin-right: 20px;">
-        </a>
-      </li>            
-        ${links
-        .map(
-          ({ page, label }) => `
-                <li>
-                  <button
-                    class="nav-btn ${page === active ? "active" : ""}"
-                    data-page="${page}">
-                    ${label}
-                    </button>
-                </li >
-              `
-      )
-      .join("")}
-      <li><button class="logout-btn" id="btn-logout">Sair</button></li>
+        <button class="btn-logout">Sair</button>
       </ul>
-  </nav >
-    `;
-
-
+    </nav>
+  `;
 
   /* Navegação */
   container.querySelectorAll("[data-page]").forEach(btn => {
@@ -74,12 +73,11 @@ export function renderNavbar({ active }) {
   });
 
   /* Logout */
-  const logoutBtn = container.querySelector("#btn-logout");
-  if (logoutBtn) {
-    logoutBtn.addEventListener("click", logout);
-  }
+  container.querySelectorAll(".btn-logout").forEach(btn => {
+    btn.addEventListener("click", logout);
+  });
 
-
+  /* Sidebar */
   window.showSidebar = function () {
     document.querySelector(".sidebar").style.display = "flex";
   };
